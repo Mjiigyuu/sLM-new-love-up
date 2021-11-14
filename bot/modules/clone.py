38c4b3802fb081056b78fde68e979a94237ff4a9
Yuuuuub88 <94279317+Yuuuuub88@users.noreply.github.com>
2021-11-14 12:51:20 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -61,14 +61,14 @@ def cloneNode(update, context):
         else:
             uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
         if uname is not None:
-            cc = f'\n\ncc: {uname}'
+            cc = f'\n\nClone BY 👉 :💃: {uname}'
             men = f'{uname} '
         if button in ["cancelled", ""]:
             sendMessage(men + result, context.bot, update)
         else:
             sendMarkup(result + cc, context.bot, update, button)
     else:
-        sendMessage('Provide G-Drive Shareable Link to Clone.', context.bot, update)
+        sendMessage('Provide G-Drive Shareable Link to Clone. 🎅🎅', context.bot, update)
 
 clone_handler = CommandHandler(BotCommands.CloneCommand, cloneNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(clone_handler)
