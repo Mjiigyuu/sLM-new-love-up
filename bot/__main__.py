4305bba1b3cff2bb69be280b0f9f63a1e1f17497
karenkaez <97656516+karenkaez@users.noreply.github.com>
2022-01-13 14:06:58 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -154,9 +154,9 @@ help_string_telegraph = f'''<br>
 <b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
 help = Telegraph(access_token=telegraph_token).create_page(
-        title='Slam Mirrorbot Help',
-        author_name='Slam Mirrorbot',
-        author_url='https://github.com/SlamDevs/slam-mirrorbot',
+        title='Join 👉 Mirrordrive Help',
+        author_name='Mirrordrive',
+        author_url='https://t.me/MirrorDrive',
         html_content=help_string_telegraph,
     )["path"]
 
