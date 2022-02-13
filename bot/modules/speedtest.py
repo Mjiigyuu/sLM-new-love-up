4793d16e44903ddec5b8c77e4e6074c6d72bc731
Musiokg <98704055+Musiokg@users.noreply.github.com>
2022-02-13 12:25:03 +0530

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -7,7 +7,7 @@ from telegram.ext import CommandHandler
 
 
 def speedtest(update, context):
-    speed = sendMessage("Running Speed Test..⚡🎀⚡ ", context.bot, update)
+    speed = sendMessage("🤣 Running Speed Test..⚡🎀⚡ ", context.bot, update)
     test = Speedtest()
     test.get_best_server()
     test.download()
@@ -15,13 +15,13 @@ def speedtest(update, context):
     test.results.share()
     result = test.results.dict()
     string_speed = f'''
-<b>Server</b>
+<b>   🎀 Server 📌</b>
 <b>🌟 Name ✅ : </b> <code>{result['server']['name']}</code>
 <b>🛠 Country 🌐: </b> <code>{result['server']['country']}, {result['server']['cc']}</code>
 <b>💐 @Mirrordrive 💝: </b> <code>{result['server']['sponsor']}</code>
 <b>🔥 ISP-5G 📶 : </b> <code>{result['client']['isp']}</code>
 
-<b>💘 Speed Test Results 🥳 - Join: @PREMIUMRELEASE</b>
+<b>💘 Speed Test Results 🥳</b>
 <b>⏫ Upload 💓 :</b> <code>{speed_convert(result['upload'] / 8)}</code>
 <b>⏬ Download 💞 :</b>  <code>{speed_convert(result['download'] / 8)}</code>
 <b>😤 Ping 💢 :</b> <code>{result['ping']} ms</code>
