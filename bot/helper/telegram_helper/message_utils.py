e6ad8f0c90bb49c54d4fad8c7ad737033030711c
Musiokg <98704055+Musiokg@users.noreply.github.com>
2022-02-13 11:08:00 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -132,7 +132,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>💽FREE:</b> <code>{free}</code> | <b>🧭UPTIME♊:</b> <code>{currentTime}</code>\n<b>⏬DL:</b> <code>{dlspeed}/s</code> 🔻 | <b>⏫UL:</b> <code>{ulspeed}/s</code> 🔺\n"
+        progress += f"\n<b>💽FREE🌼:</b> <code>{free}</code> | <b>🧭UPTIME♊:</b> <code>{currentTime}</code>\n<b>⏬DL:</b> <code>{dlspeed}/s</code> 🔻 | <b>⏫UL:</b> <code>{ulspeed}/s</code> 🔺\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
